22d77128f1d2f7ff8ced669c240098a02fe27e29
Lukaish <89522884+Lukaish@users.noreply.github.com>
2021-11-17 15:09:22 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'bart.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEF',
+                fotos: [
+                    {url: 'bobEsponja.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ],
+    },
+    {
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclanio',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url: 'irmao-do-kiki.jfif', legenda: 'Prefeito'},
+                    {url: 'marge.png', legenda: 'Vice-Prefeito', small: true}
+                ],
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'RST',
+                vice: 'Zul',
+                fotos:[
+                    {url: 'south.jfif', legenda: 'Prefeito'},
+                    {url: 'mascara.jfif', legenda: 'Vice-Prefeito', small: true}
+                ],  
+            }
+        ],
+    },
+];
